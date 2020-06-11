7734da940be386e51827e1651c656b04c2402660
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-06-11 15:28:50 -0700

    hopefully updating models to be correct for the final time

---
+++ __tests__/memberships.test.js
@@ -0,0 +1,14 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+const Membership = require('../lib/models/Membership');
+
+describe('membership routes', () => {
+
+
+
+});
